5183e573afa581047ecec6719864fe3628de5ffb
gonzabl <gonzabl@live.com.ar>
2020-12-31 13:30:53 -0300

    Ganar el nivel
    Agregado ademas pared y un rediseño del nivel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,4 +19,9 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    public void CompleteLevel()
+    {
+        Debug.Log("Level Won!");
+    }
 }
